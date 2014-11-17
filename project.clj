dfa56899b567ad1bf5b61d29401b0d582d2a0135
tangrammer <juanantonioruz@gmail.com>
2014-11-17 09:23:45 +0100

    Update tests

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tangrammer/co-dependency "0.1.1"
+(defproject tangrammer/co-dependency "0.1.2-SNAPSHOT"
   :description "Co-dependency relation using stuartsierra/component library. Co-dependency idea and meaning taken from juxt/component"
   :url "https://github.com/tangrammer/co-dependency"
   :license {:name "Eclipse Public License"
--- test/tangrammer/co_dependency_test.clj
+++ test/tangrammer/co_dependency_test.clj
@@ -3,26 +3,28 @@
             [com.stuartsierra.component :as component]
             [tangrammer.co-dependency :refer (co-using) :as co-dependency]))
 
+(defn- create-state [k]
+  (str "state " k ": "  (rand-int Integer/MAX_VALUE)))
 
 (defrecord ComponentA [state])
 
-(defn component-a [] (->ComponentA "state A"))
+(defn component-a [] (->ComponentA {:state (create-state :a)}))
 
 (defrecord ComponentB [state a])
 
-(defn component-b [] (map->ComponentB {:state "state B"}))
+(defn component-b [] (map->ComponentB {:state (create-state :b)}))
 
 (defrecord ComponentC [state a b])
 
-(defn component-c [] (map->ComponentC {:state "state C"}))
+(defn component-c [] (map->ComponentC {:state (create-state :c)}))
 
 (defrecord ComponentD [state my-c b])
 
-(defn component-d [] (map->ComponentD {:state "state D"}))
+(defn component-d [] (map->ComponentD {:state (create-state :d)}))
 
 (defrecord ComponentE [state])
 
-(defn component-e [] (map->ComponentE {:state "state E"}))
+(defn component-e [] (map->ComponentE {:state (create-state :e)}))
 
 (defrecord System1 [d a e c b]  ; deliberately scrambled order
   component/Lifecycle
@@ -44,9 +46,11 @@
                         (component/using {:b :b :my-c :c}))
                  :e (component-e)}))
 
+
+
 (deftest basic-test
   (testing ":b depends on :a, :a co-depends on :b"
     (let [co-s (co-dependency/start (system-1))]
-      (is (= (:state (:a co-s)) (:state (:a ((-> co-s :a :b)))) "state A"))
-      (is (= (:state (:a co-s)) (:state (:a ((-> co-s :a :c )))) "state A"))
-      (is (= (:state (:a co-s)) (:state (:a (:my-c ((-> co-s :a :d))))) "state A")))))
+      (is (= (:state (:a co-s)) (:state (:a ((-> co-s :a :b))))))
+      (is (= (:state (:a co-s)) (:state (:a ((-> co-s :a :c ))))))
+      (is (= (:state (:a co-s)) (:state (:a (:my-c ((-> co-s :a :d))))))))))
