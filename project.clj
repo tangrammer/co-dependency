466b0ac259585a51ff92e475096dcba3b3ebef58
tangrammer <juanantonioruz@gmail.com>
2014-11-21 11:09:22 +0100

    Prepare for develoment version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject tangrammer/co-dependency "0.1.3"
+(defproject tangrammer/co-dependency "0.1.4-SNAPSHOT"
   :description "Co-dependency relation using stuartsierra/component library. Co-dependency idea and meaning taken from juxt/component"
   :url "https://github.com/tangrammer/co-dependency"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.stuartsierra/component "0.2.2"]])
